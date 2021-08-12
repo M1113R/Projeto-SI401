17d6479e0d00d20909708c94c3075a20b20fc1b8
M1113r <g234930@dac.unicamp.br>
2021-08-12 20:20:15 -0300

    first

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto</title>
+</head>
+<body>
+    <div class="frmLogin">
+        <form action="" method="post">
+            <label for="login">Login:</label>
+            <input type="text" name="login" id="login">
+            
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password">
+            <input type="button" value="submit">
+        </form>
+    </div>
+</body>
+</html>
