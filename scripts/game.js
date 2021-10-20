c9d6e62b57e0c48b201a1afe39cea70e9d755ac1
Fischer <fischerrobson@gmail.com>
2021-10-19 21:24:24 -0300

    correcao no preenchimento das posicoes das bombas, atualizacao dos numeros de acordo com as posicoes das bombas e funcao de clicar e revelar o conteudo do elemento

--- scripts/game.js
+++ scripts/game.js
@@ -8,12 +8,18 @@ let boardParams = {
   bombsPositions: [],
 }
 
+const AROUND_CELL_OPERATORS = [
+  [-1, -1], [-1, 0], [-1, 1],
+  [0, -1], [0, 1],
+  [1, -1], [1, 0], [1, 1],
+];
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function isAlreadyAMine(minePosition) {
-  return boardParams.bombsPositions.join("").includes(minePosition.toString());
+function verifyIfPositionIsABomb(position) {
+  return boardParams.bombsPositions.join("").includes(position.toString());
 }
 
 function generateNewBoard() {
@@ -25,8 +31,7 @@ function generateNewBoard() {
   }
 
   generateBombsPositionsAndInsert();
-
-  console.table(boardParams.board);
+  updateBoardNumbers();
 
   const board = document.querySelector(".board");
 
@@ -37,7 +42,16 @@ function generateNewBoard() {
 
     for (let y = 0; y < boardParams.rows; y++) {
       const $cell = document.createElement('SPAN');
-      $cell.innerHTML = boardParams.board[x][y];
+      //const printableValue = boardParams.board[x][y];
+      const printableValue = "";
+      $cell.innerHTML = printableValue;
+      $cell.id = `${x}-${y}`;
+      if (printableValue === "💣") $cell.classList.add('bomb');
+      $cell.addEventListener("click", function printValue() {
+        const element = document.getElementById(`${x}-${y}`);
+        element.innerHTML = boardParams.board[x][y];
+        if (element.textContent === "💣") alert("Game Over");
+      });
       $row.appendChild($cell);
     }
 
@@ -45,21 +59,37 @@ function generateNewBoard() {
   }
 }
 
+
+
 function generateBombsPositionsAndInsert() {
   boardParams.bombsPositions = [];
   while (boardParams.bombsPositions.length < boardParams.bombs) {
     const y = getRandomInt(0, boardParams.rows);
     const x = getRandomInt(0, boardParams.columns);
-
-    //if (isAlreadyAMine([y, x])) {
-    boardParams.bombsPositions.push([y, x]);
-    //}
+    if (!verifyIfPositionIsABomb([x, y])) boardParams.bombsPositions.push([x, y]);
   }
 
   for (let i = 0; i < boardParams.bombsPositions.length; i++) {
     const y = boardParams.bombsPositions[i][0];
     const x = boardParams.bombsPositions[i][1];
-    boardParams.board[y][x] = "B";
+    boardParams.board[y][x] = "💣";
+  }
+}
+
+function updateBoardNumbers() {
+  for (let i = 0; i < boardParams.bombsPositions.length; i++) {
+    for (let j = 0; j < AROUND_CELL_OPERATORS.length; j++) {
+      const minePosition = boardParams.bombsPositions[i];
+      const around = AROUND_CELL_OPERATORS[j];
+      const boardY = minePosition[0] + around[0];
+      const boardX = minePosition[1] + around[1];
+
+      if (boardY >= 0 && boardY < boardParams.rows &&
+        boardX >= 0 && boardX < boardParams.columns &&
+        typeof boardParams.board[boardY][boardX] === 'number') {
+        boardParams.board[boardY][boardX]++;
+      }
+    }
   }
 }
 
